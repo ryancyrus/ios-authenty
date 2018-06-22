a2cd2bd13aaada501ff9ba010d08ec46555e91be
Ryan Cyrus <ryan.cyrus@live.com>
2018-06-22 18:16:06 -0400

    Added functionality to check if valid email and password was entered.

--- Authentication/ViewController.swift
+++ Authentication/ViewController.swift
@@ -119,14 +119,55 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
     }
     
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "(?:[a-zA-Z0-9!#$%\\&‘*+/=?\\^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%\\&'*+/=?\\^_`{|}" +
+        "~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\" +
+        "x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-" +
+        "z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5" +
+        "]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-" +
+        "9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21" +
+        "-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES[c] %@", emailRegEx)
+        return emailTest.evaluate(with: email)
+    }
+    
+    func checkErrorsOnEmailInput(email: String) -> String{
+        if email.isEmpty{
+            
+            return "Please enter an email address."
+        }else if !isValidEmail(email){
+            return "Please enter a valid email address"
+        }
+        
+        return ""
+    }
+    
+    func flagEmail(email:String){
+        let error = checkErrorsOnEmailInput(email: email)
+        if !error.isEmpty{
+            emailField.layer.borderColor = UIColor.red.cgColor
+            emailField.layer.borderWidth = 1.0
+        }
+    }
+    
+    func flagPassword(password:String){
+        if password.isEmpty{
+            passwordField.layer.borderColor = UIColor.red.cgColor
+            passwordField.layer.borderWidth = 1.0
+        }
+    }
+    
     @objc func dismissKeyboard(){
         view.endEditing(true)
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if textField == emailField {
+            flagEmail(email: emailField.text!)
             self.passwordField.becomeFirstResponder()
         }else{
+            flagPassword(password: passwordField.text!)
             self.passwordField.resignFirstResponder()
         }
         return true
